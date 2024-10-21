f973b42fb1119e2d420c11151d3a5f6b6261e19f
Tuneer Saha <tuneer.work@gmail.com>
2024-10-22 01:03:52 +0530

    third commit

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from ultralytics import YOLO
 import cv2
 import math 
-# start webcam
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-# model
 model = YOLO("yolo-Weights/yolov8n.pt")
 
-# object classes
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
               "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
@@ -27,27 +24,17 @@ while True:
     success, img = cap.read()
     results = model(img, stream=True)
 
-    # coordinates
     for r in results:
         boxes = r.boxes
 
         for box in boxes:
-            # bounding box
             x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
-
-            # put box in cam
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-
-            # confidence
             confidence = math.ceil((box.conf[0]*100))/100
             print("Confidence --->",confidence)
-
-            # class name
             cls = int(box.cls[0])
             print("Class name -->", classNames[cls])
-
-            # object details
             org = [x1, y1]
             font = cv2.FONT_HERSHEY_SIMPLEX
             fontScale = 1
